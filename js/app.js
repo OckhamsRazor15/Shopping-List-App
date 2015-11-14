d65b4635fc7bfea879a4cbe64f534ae993df1ced
Sako Kassabian <skassabian@csmci.com>
2015-11-13 21:02:30 -0800

    adds event handlers for checking and removing items.

--- js/app.js
+++ js/app.js
@@ -1,12 +1,24 @@
 $(document).ready(function(){
+// adds item to list
 $('form').submit(function(e){
 	var $itemval = $("#additem").val();
-	var $item = $("<li class='item'><input type=\"checkbox\"> "+$itemval+"</li>");
+	var $item = $("<li class='item'><input class='itemcheck' type='checkbox'> "+$itemval+"<button class='rmvbutton'>Remove</button></li>");
 	e.preventDefault();
 
-	$(".itemlist").append($item);
-	
+	$(".itemlist").append($item);	
 });
+// crosses out item when checked
+$('.itemlist').on('click', "li.item", function(){
+if ($(this).children().is(":checked")){
+  $(this).parent().css("text-decoration", "line-through");
+} else {
+	$(this).parent().css("text-decoration", "none");
+}
 
+});
+//removes an item when the remove button is clicked
+$(".itemlist").on("click", ".rmvbutton", function(){
+$(this).parent().remove();
 
+});
 });
